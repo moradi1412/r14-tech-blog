161d0743db810fd5112fecec0c6a294f7ba98527
Moradi1412 <moradi.javid@gmail.com>
2021-12-11 17:14:47 -0800

    created the routes and tested api

---
+++ controllers/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+const apiRoutes = require('./api/');
+const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashborad-routes.js');
+
+router.use('/dashboard', dashboardRoutes);
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+
+module.exports = router;
