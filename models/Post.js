7abcef5b152866851bed6a6554f861f7af984c43
Moradi1412 <moradi.javid@gmail.com>
2021-12-11 16:21:11 -0800

    added post model and index.js for all models

---
+++ models/Post.js
@@ -0,0 +1,28 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+
+// create our Post model
+class Post extends Model {}
+
+// create fields/columns for Post model
+Post.init(
+  {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    body: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post'
+  }
+);
+
+module.exports = Post;
---
+++ models/index.js
@@ -0,0 +1,9 @@
+// import all models
+//const Post = require('./Post');
+const User = require('./User');
+//const Comment = require('./Comment');
+
+// create associations
+
+
+module.exports = { User };
